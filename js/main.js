08db192cbed97d4e94665c02b381fc369519cdf4
Muver1337 <muvers1s@gmail.com>
2023-03-15 11:15:40 +0700

    zadanie2.3

--- js/main.js
+++ js/main.js
@@ -1,5 +1,143 @@
 let eventBus = new Vue()
 
+Vue.component('cols', {
+    template: `
+    <div id="cols">
+    <div class="col-wrapper">
+    <h2 class="error" v-for="error in errors">{{error}}</h2>
+        <create></create>
+        <div class="cols-wrapper">
+            <div class="col">
+                <ul>
+                    <li class="cards" style="background-color: #e79ba2" v-for="card in column1"><p class="p-title">{{ card.title }}</p>
+                        <ul>
+                            <li class="tasks" v-for="t in card.subtasks" v-if="t.title != null">
+                                <input @click="newStatus1(card, t)"
+                                class="checkbox" type="checkbox"
+                                :disabled="t.completed">
+                                <p :class="{completed: t.completed}">{{t.title}}</p>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+`,
+    data() {
+        return {
+            column1: [],
+            column2: [],
+            column3: [],
+            errors: [],
+        }
+    },
+
+    mounted() {
+        eventBus.$on('card-submitted', card => {
+            this.errors = []
+            if (this.column1.length < 3) {
+                this.column1.push(card)
+            } else {
+                this.errors.push("You can't add a new card now.")
+            }
+        })
+    },
+    methods: {
+        newStatus1(card, t) {
+            t.completed = true
+            let count = 0
+            card.status = 0
+            this.errors = []
+            for (let i = 0; i < 5; i++) {
+                if (card.subtasks[i].title != null) {
+                    count++
+                }
+            }
+
+            for (let i = 0; i < count; i++) {
+                if (card.subtasks[i].completed === true) {
+                    card.status++
+                }
+            }
+            if (card.status / count * 100 >= 50 && card.status / count * 100 < 100 && this.column2.length < 5) {
+                this.column2.push(card)
+                this.column1.splice(this.column1.indexOf(card), 1)
+            } else if (this.column2.length === 5) {
+                this.errors.push('You need to complete card in the second column to add new card or complete card in the first column')
+                if (this.column1.length > 0) {
+                    this.column1.forEach(item => {
+                        item.subtasks.forEach(item => {
+                            item.completed = true;
+                        })
+                    })
+                }
+            }
+        },
+        newStatus2(card, t) {
+            t.completed = true
+            let count = 0
+            card.status = 0
+            for (let i = 0; i < 5; i++) {
+                if (card.subtasks[i].title != null) {
+                    count++
+                }
+            }
+
+            for (let i = 0; i < count; i++) {
+                if (card.subtasks[i].completed === true) {
+                    card.status++
+                }
+            }
+            if (card.status / count * 100 === 100) {
+                this.column3.push(card)
+                this.column2.splice(this.column2.indexOf(card), 1)
+                card.date = new Date()
+            }
+            if (this.column2.length < 5) {
+                if (this.column1.length > 0) {
+                    this.column1.forEach(item => {
+                        item.subtasks.forEach(item => {
+                            item.completed = false;
+                        })
+                    })
+                }
+            }
+        }
+    },
+
+    computed: {},
+    props: {
+        card: {
+            title: {
+                type: Text,
+                required: true
+            },
+            subtasks: {
+                type: Array,
+                required: true,
+                completed: {
+                    type: Boolean,
+                    required: true
+                }
+            },
+            date: {
+                type: Date,
+                required: false
+            },
+            status: {
+                type: Number,
+                required: true
+            },
+            errors: {
+                type: Array,
+                required: false
+            }
+        },
+    },
+})
+
 Vue.component('create', {
     template: `
     <section>
@@ -49,10 +187,6 @@ Vue.component('create', {
         }
     }
 })
-
-
-
-
 
 
 let app = new Vue({
