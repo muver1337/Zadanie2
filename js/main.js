028e332ca64989e63b4250eab11f346b4a9c8b2d
Muver1337 <muvers1s@gmail.com>
2023-03-15 14:19:05 +0700

    zadanie2.7

--- js/main.js
+++ js/main.js
@@ -170,11 +170,11 @@ Vue.component('create', {
                 <input id="title" required v-model="title" type="text" placeholder="Заголовок">
             </p>
             <input required id="subtask1" v-model="subtask1" placeholder="Задача 1">
-            <input required id="subtask2" v-model="subtask2" maxlength="30" placeholder="Задача 2">
-            <input required id="subtask3" v-model="subtask3" maxlength="30" placeholder="Задача 3">
-            <input  id="subtask4" v-model="subtask4" maxlength="30" placeholder="Задача 4">
-            <input  id="subtask5" v-model="subtask5" maxlength="30" placeholder="Задача 5">
-            <button type="submit">Добавить</button>
+            <input required id="subtask2" v-model="subtask2" maxlength="25" placeholder="Задача 2">
+            <input required id="subtask3" v-model="subtask3" maxlength="25" placeholder="Задача 3">
+            <input  id="subtask4" v-model="subtask4" maxlength="25" placeholder="Задача 4">
+            <input  id="subtask5" v-model="subtask5" maxlength="25" placeholder="Задача 5">
+            <button type="submit">Добавить задачу</button>
         </form>
     </section>
     `,
@@ -216,6 +216,6 @@ Vue.component('create', {
 let app = new Vue({
     el: '#app',
     data: {
-        name: 'Записи'
+        name: 'Добавление записи'
     }
 })
